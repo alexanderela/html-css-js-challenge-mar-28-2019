6330aa05a05e2d343f471b0490163df7edd5ec6a
Alexander Ela <ela.alexander@gmail.com>
2019-03-28 23:47:34 -0600

    Challenge 2. Javascript Basics

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*****************************************************************************************
 * Part 2
 ****************************************************************************************/
-var employees = [
+const employees = [
         {first: "Amanda", last: "Byron", group: "Sales"},
         {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
         {first: "Miltiades", last: "Crescens", group: "Sales"},
@@ -9,6 +9,34 @@ var employees = [
     ];
 
 // Part 2 Answer Here
+
+const organizeByGroup = (employeesList) => {
+  const sortedEmployees = employeesList.sort((a, b) => {
+    const groupA = a.group.toUpperCase();
+    const groupB = b.group.toUpperCase();
+    if(groupA < groupB) {
+      return -1;
+    }
+    if(groupA > groupB) {
+      return 1;
+    }
+    return 0
+  });
+
+  const employeesByGroup = sortedEmployees.reduce((groupsObj, employee) => {
+    if(!groupsObj[employee.group]) {
+      groupsObj[employee.group] = []
+    }; 
+    
+    groupsObj[employee.group].push({'name': `${employee.first} ${employee.last}`});
+    
+    return groupsObj;
+  }, {});
+
+  return employeesByGroup;
+}
+
+organizeByGroup(employees)
 
 /*****************************************************************************************
 * Bonus
